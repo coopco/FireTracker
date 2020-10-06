6e2e7d285b4745b6bd964795e26d9e9baa779c39
ronniebarker23 <56721682+ronniebarker23@users.noreply.github.com>
2020-10-06 15:13:36 +0100

    Take out embedded trend page code

--- app.R
+++ app.R
@@ -8,46 +8,20 @@ library(geojsonio)
 library(htmltools)
 library(ggplot2)
 library(rgdal)
+library(plotly)
 source("map.R", local=TRUE)
-
-nFires <- read.csv("data/CorrelationData.csv")
-nFires$Date <- as.Date(nFires$Date)
+source("PreviousTrends.R", local=TRUE)
 
 ui <- bootstrapPage(
   navbarPage(theme=shinytheme("flatly"), collapsible = TRUE, id="nav", title="Fire Tracker",
     mapTab,
-    tabPanel("Previous trends",
-      #tabs to select data in graph
-      selectInput("State", "State:",
-                c("All" = "ALL",
-                  "Queensland" = "QLD",
-                  "Victoria" = "VIC",
-                  "New South Wales" = "NSW",
-                  "Northern Territory" = "NT",
-                  "South Australia" = "SA",
-                  "Tasmania" = "TAS")),
-      #show correlation plot
-      plotOutput("corrPlot", width = "50%")
-    )
+    TrendsTab
   )
 )
 
 server <- function(input, output) {
   mapServer(input, output)
-  
-  #correlation plot
-  output$corrPlot <- renderPlot({
-    stateName <- input$State  #selected data
-    selectedData <- nFires
-    if(stateName!="ALL"){ #if not all states, take subset of data
-      selectedData <- nFires[which(nFires$State==stateName),]  #just that state 
-    }
-    
-    scatter <- ggplot(selectedData, aes(x=Date, y=nFire, color = Temperature)) +
-      geom_point(alpha=0.7) + ggtitle(paste("Number of Fires and Temperature Over Time in "), stateName) +
-      ylab("Number of Fires") + xlab("Month")
-    scatter + scale_color_gradient(low="white", high="red")
-    
+  TrendsServer(input,output)  
   })
 }
 
