85879849aabf420dc9cc2ee24ee9fdbeeeaef45c
ronniebarker23 <56721682+ronniebarker23@users.noreply.github.com>
2020-10-25 09:08:36 +0000

    Add files via upload

---
+++ scripts/clean weather.R
@@ -0,0 +1,29 @@
+#CLEAN WEATHER DATA - RHONWYN
+
+setwd("/Users/ronniebarker/Documents/Uni/3164")
+weather <- read.csv('weatherdata.csv') #read in
+attach(weather)  #use c names
+
+weather <- weather[,c(1,2,4,5,12,13,14,15)]  #select relevant columns (only max temp, min of humidity, generic wind speed)
+weather$Humidity <- with(weather, pmin(weather$Humidity9am, weather$Humidity3pm)) #find min humidity
+weather$WindSpeed <- with(weather, pmax(weather$WindSpeed9am, weather$WindSpeed3pm))
+weather <-weather[,-c(5,6,7,8)] #remove timed humidity and wind
+
+#Check if any locations have too many na and should be omitted 
+library(dplyr)
+tmp <- weather %>% group_by(weather$Location) %>% summarise_all(funs(sum(is.na(.))))  #number of na per column grouped bylocation
+tmp <-tmp[,-c(2,3)] #remove irrelevant
+tmp$Sum <- tmp[2]+tmp[3]+tmp[4] #sum na per location
+colnames(tmp)<- c("Location", "MaxTemp", "Rainfall", "Humidity", "Total")
+count<- weather %>%
+  count(Location) #count of entries per location
+colnames(count)  <- c("Location", "Total Obs")
+tmp <- merge(tmp, count)  #merge
+tmp$Percent <- (tmp[5]/tmp[6])*100 #percent of entries that are na per location 
+
+summary(weather)
+weather[!complete.cases(weather),]
+
+weather <- na.omit(weather) #remove na
+
+write.csv(weather, "cleanedWeather.csv")
---
+++ scripts/cleanFire.R
@@ -0,0 +1,29 @@
+setwd("/Users/ronniebarker/Documents/Uni/3164/bushfire")
+fire1 <- read.csv('fire_archive_M6_145466.csv')
+fire2 <- read.csv('fire_archive_V1_145468.csv')
+fire3 <- read.csv('fire_nrt_J1V-C2_145467.csv')
+fire4 <- read.csv('fire_nrt_M6_145466.csv')
+fire5 <- read.csv('fire_nrt_V1_145468.csv')
+
+#see what columns
+colnames(fire1)
+colnames(fire2)
+colnames(fire3)
+colnames(fire4)
+colnames(fire5)
+
+#pre-process make have same columns
+fire1 <- fire1[,-c(3,12,14,15)] #remove brightness, type and daynight
+fire2 <- fire2[,-c(3,12,14)] #remove brightness and type
+fire3 <- fire3[,-c(3,12,14)] #remove brightness and daynight
+fire4 <- fire4[,-c(3,12,14)] #remove brightness and daynight
+fire5 <- fire5[,-c(3,12,14)] #remove brightness and 
+
+#join
+fire <- rbind(fire1, fire2)
+fire <- rbind(fire, fire3)
+fire <- rbind(fire, fire4)
+fire <- rbind(fire, fire5)
+
+#export
+write.csv(fire, "cleanFire.csv")
